bf0bfea5a3519346bd598feb91cf36d434eedf6b
LilJade09 <josue2duran@hotmail.com>
2020-05-28 09:39:55 -0600

    Proyecto..

---
+++ HolaMundo/src/main/java/com/lilJade/HolaMundo/controlador.java
@@ -0,0 +1,17 @@
+package com.lilJade.HolaMundo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class controlador {
+
+	@GetMapping("/")
+	public String Inicio(Model modelo) {
+		String msj = "Hola desde el controlador";
+		modelo.addAttribute("saludo", msj);
+		return "index";
+	}
+	
+}
